22bc83e4112402f7f76f241370b3d3901c564261
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2024-02-22 19:20:16 -0400

    Add files via upload
    Questão um do teste

---
+++ questao_um.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int indice = 13;
+    int soma = 0;
+    int k = 0;
+    
+    while(k < indice){
+        k = k + 1;
+        soma = soma + k;
+    }
+    
+    std::cout << soma << std::endl;
+
+    return 0;
+}
